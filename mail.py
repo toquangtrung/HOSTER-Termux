e4be48896a2eb96be7fdb56ed99722704ffc5c09
rexdevin8 <67311738+rexdevin8@users.noreply.github.com>
2022-03-21 23:10:13 +0530

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,24 @@
+# hoster_by_DiyRex@outlook.com:Hoster1234
+
+import smtplib
+
+def sendmail(email):
+    with open("checked_hosts.txt") as file:
+        list = []
+        for line in file:
+            list.append(line)
+
+    body = 'Subject: Hoster .\nDear ContactName, \n\n' + '\n'.join(list)
+    try:
+        smtpObj = smtplib.SMTP('smtp-mail.outlook.com', 587)
+    except Exception as e:
+        print(e)
+        smtpObj = smtplib.SMTP_SSL('smtp-mail.outlook.com', 465)
+    #type(smtpObj) 
+    smtpObj.ehlo()
+    smtpObj.starttls()
+    smtpObj.login('hoster_by_DiyRex@outlook.com', "Hoster1234") 
+    smtpObj.sendmail('hoster_by_DiyRex@outlook.com', email, body) # Or recipient@outlook
+
+    smtpObj.quit()
+    pass
